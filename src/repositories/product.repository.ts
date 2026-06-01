1143fd43eb1d9c6e6ce0b7ab0752a988b7d6096f
Rafael Fontes <rft180407@gmail.com>
2026-05-31 23:03:05 -0300

    feat: implement product repository methods for create, update, soft delete, and find available products

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,13 +1,43 @@
+import { Prisma } from "../generated/prisma";
 import prisma from "../lib/prisma";
 
+export async function create(sku: string, name: string, price: number, stock: number, storeId: string, description?: string, costPrice?: number, categoryId?: string){
+    return prisma.product.create({
+        data: {
+            sku,
+            name,
+            description,
+            price,
+            costPrice,
+            stock,
+            categoryId,
+            storeId,
+        },
+    });
+}
+
+export async function update(id: string, data: Prisma.ProductUpdateInput) {
+    return prisma.product.update({
+        where: { id },
+        data,
+    });
+}
+
+export async function softDelete(id: string){
+    return prisma.product.update({
+        where: { id },
+        data: {available: false}
+    });
+}
+
 export async function findAll(){
-    return prisma.product.findMany()
+    return prisma.product.findMany({
+        where: { available: true}
+    });
 }
 
 export async function findById(id:string){
     return prisma.product.findUnique({
-        where: {
-            id: id,
-        },
+        where: { id }
     });
 }
